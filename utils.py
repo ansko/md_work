fe446ee85f90d49abbb78b52d85953cbb46acc9f
ansko <antonskomorokhov@gmail.com>
2019-05-17 21:24:17 +0300

    add change_atoms into utils (mayby worthless)

--- utils.py
+++ utils.py
@@ -4,3 +4,37 @@ def move_some_atoms(dfc, atoms_idcs_list, dx=0, dy=0, dz=0):
             atom['x'] += dx
             atom['y'] += dy
             atom['z'] += dz
+
+
+def change_atoms(dfc, condition, remap):
+    done = 0
+    for atom in dfc.atoms:
+        try:
+            atom['type'] = remap[condition(atom)]
+            done += 1
+        except KeyError:
+            pass
+    return done
+
+
+if __name__ == '__main__':
+   from datafile_content import DatafileContent
+
+   dfc = DatafileContent('na_mmt.125000.data')
+
+   # into atom labels:
+   condition = lambda atom: str(atom['q'])
+   remap = {
+       '1.575':      'Al',
+       '1.36':       'Mg',
+       '2.1':        'Si',
+       '-1.05':       'O',
+       '-1.0808':     'O',
+       '-0.95':       'O',
+       '-1.1688':     'O',
+       '0.4245':      'H',
+       '1.0':         'Na'
+   }
+   print(change_atoms(dfc, condition, remap))
+
+   dfc.write('000.data')
